d43de283bdd93a879d2a79e6247078ac45825781
DonatoBleve <s281553@studenti.polito.it>
2025-02-21 21:08:34 +0100

    Fix js bro

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -1,22 +1,30 @@
 import path from 'path';
 
 export default {
+    mode: 'development', // Set the mode to 'development' or 'production'
     entry: './src/main.tsx',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve('dist'),
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
     module: {
         rules: [
             {
                 test: /\.tsx?$/,
                 use: 'ts-loader',
-                exclude: /node_modules/,
+                exclude: [/node_modules/, /\.d\.ts$/], // Exclude .d.ts files
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                type: 'asset/resource',
             },
         ],
     },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve('dist'),
-    },
-    mode: 'development',
+    devtool: 'source-map',
 };
