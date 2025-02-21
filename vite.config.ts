a0452669e9463e645480bf19624dbac61fb60f21
DonatoBleve <s281553@studenti.polito.it>
2025-02-21 20:32:08 +0100

    Fix base path and script source

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import './src/main.tsx';
 
 export default defineConfig({
   base: '/crimes/', // Assicurati di impostare il percorso base corretto
